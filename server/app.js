c872de0831cafc529d5ecebd68a85fdf9ca91379
Chrismarcel <cmarcelj@gmail.com>
2019-05-10 13:03:12 +0100

    feat: set up backend environment
    - install required dependencies
    - set up directory and folder structure
    - set up husky for git pre-commit hooks and linting
    - set up server instance using express

---
+++ server/app.js
@@ -0,0 +1,35 @@
+import '@babel/polyfill';
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+// Enable CORS middleware for managing Cross-Origin Resource Sharing
+app.use(cors());
+
+// Log requests to the console. Necessary for debugging
+app.use(logger('dev'));
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+
+// Setup a generic welcome message for the '/' route
+app.get('/', (req, res) => res.status(200).send({
+  message: 'Welcome to Shopmate'
+}));
+
+// Return an error 404 for non-existent routes
+app.use((req, res) => {
+  res.status(404).send({ message: 'Route not found' });
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port);
+
+export default app;
